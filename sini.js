7e99a6fd35cfcd4767a77ee36706476b3c728c98
Sini T P <sinitp2015@gmail.com>
2021-05-02 12:44:04 +0530

    first commit

---
+++ sini.js
@@ -0,0 +1,32 @@
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+
+const options = {
+    threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+
+function navCheck(entries){
+    entries,forEach(entry =>{
+        const className = entry.target.className;
+        const activeAnchor = document.querySelector('[data-page = ${classNamw}]');
+        const gradientIndex = entry.target.getAttribute("data-index");
+        const coords = activeAnchor.getBoundingClientRect();
+        const directions = {
+            height: coords.height , width: coords.width , top: coords.top , left: coords.left
+        };
+        if(entry.isIntersecting) {
+            bubble.style.setProperty("left",'${directions.left}px' );
+            bubble.style.setProperty("top",'${directions.top}px' );
+            bubble.style.setProperty("width",'${directions.width}px' );
+            bubble.style.setProperty("height",'${directions.height}px' );
+        }
+    });
+
+}
+
+sections.forEach(section => {
+    observer.observe(section);
+});
